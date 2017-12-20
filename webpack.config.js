8929d5625cc5ffb028bbd17a3d48b8bc665d4cdb
chloedevriese <chloe.devriese@student.howest.be>
2017-12-20 04:13:20 +0100

    end

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const {ifProduction, ifDevelopment} = getIfUtils(process.env.NODE_ENV);
 const extractCSS = new ExtractTextWebpackPlugin(`css/style.css`);
 
 // change for production build on different server path
-const publicPath = `http://student.howest.be/chloe.devriese/20172018/EXW/GRAVITY/`;
+const publicPath = ``;
 // const publicPath = `/`;
 
 const port = 3000;
